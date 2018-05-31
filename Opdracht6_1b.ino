57ea1fb3344bcfa88c01bb7262b4deaf6600c513
Luuk <31882018+CoderClassLuuk@users.noreply.github.com>
2018-05-31 09:53:48 +0200

    Opdracht 1b gemaakt van leskaart 6

---
+++ Opdracht6_1b.ino
@@ -0,0 +1,31 @@
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A2;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  float outputValueNew = (255 - outputValue);
+  // change the analog out value:
+  analogWrite(analogOutPin, outputValueNew);
+
+  // print the results to the Serial Monitor:
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValueNew);
+
+  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(2);
+}
